f33d020fcf7dea31f31d5aad5bb11aa9a2c5b9fc
Pablo <akpablo1997@gmail.com>
2017-03-02 10:37:58 -0300

    Filter finished

--- app/src/main/java/com/example/pablo/easycontacts/utils/Filters/Filter.java
+++ app/src/main/java/com/example/pablo/easycontacts/utils/Filters/Filter.java
@@ -11,7 +11,11 @@ import java.util.List;
 
 public class Filter implements InterfaceFilters<Contact> {
 
-
+    /**
+     * @param list
+     * @param name
+     * @return Lista de contatos filtrados pelo {@name}
+     */
     @Override
     public List<Contact> filterByName(List<Contact> list, String name) {
         List<Contact> responseList = new ArrayList<>();
